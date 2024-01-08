badd22fbaf4df08a23d862c5046fcef377f28a6c
manish sharma <smanish2401@gmail.com>
2024-01-08 09:42:10 +0530

    minor changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,80 +7,80 @@ import { Router } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit{
-  
-  displayedColumns: string[] = ['symbol','flag', 'name','continents','population','area','button'];
-  isLoading:boolean=false;
-  dataSource:any=[];
-  allCountries:any=[];
-  error:any;
-  searchText:any;
-  region:any;
-  
-  
-  constructor(private data:DataService, private route:Router) {}
+export class HomeComponent implements OnInit {
 
-  ngOnInit(): void{
-   this.onFetchdata()
-   
-      
-    }
-onFetchdata() {
-  this.isLoading=true
-  this.data.getAllCountries().subscribe((result)=>{
-    console.log(result);
-    if(result){
-      this.dataSource=result;
-      this.data.dataSource=this.dataSource;
-      
-      setTimeout(() => {
-        this.isLoading=false
-      }, 2000);
-    }
-    else{
-      
-      this.error="Error 404 ! Try again after some time"
-      
-    }
-    
-  })
-  
+  displayedColumns: string[] = ['symbol', 'flag', 'name', 'continents', 'population', 'area', 'button'];
+  isLoading: boolean = false;
+  dataSource: any = [];
 
-}
-onPress(){
-  this.data.getSearchByName(this.searchText).subscribe((result)=>{
-    if(result){
-      console.warn(result)
-      this.dataSource=result
-    }
-    else{
-      
-      
-      console.warn("error");
-      this.onFetchdata()
-    }
-   
-  })
-  
-}
-onClickCountry(element:any){
-  this.data.dataSource=element;
-  this.route.navigate(['country',element.flag])
+  error: any;
+  searchText: any;
+  region: any;
+
+
+  constructor(private data: DataService, private route: Router) { }
+
+  ngOnInit(): void {
+    this.onFetchdata()
+
+
+  }
+  onFetchdata() {
+    this.isLoading = true
+    this.data.getAllCountries().subscribe((result) => {
+      console.log(result);
+      if (result) {
+        this.dataSource = result;
+        this.data.dataSource = this.dataSource;
+
+        setTimeout(() => {
+          this.isLoading = false
+        }, 2000);
+      }
+      else {
+
+        this.error = "Error 404 ! Try again after some time"
+
+      }
+
+    })
+
+
+  }
+  onPress() {
+    this.data.getSearchByName(this.searchText).subscribe((result) => {
+      if (result) {
+        console.warn(result)
+        this.dataSource = result
+      }
+      else {
+
+
+        console.warn("error");
+        this.onFetchdata()
+      }
+
+    })
+
+  }
+  onClickCountry(element: any) {
+    this.data.dataSource = element;
+    this.route.navigate(['country', element.flag])
   }
 
 
-  onChange(event:any){
-let val = event.target.value
-if(val==="Filter Based on Continents"){
-  this.onFetchdata()
-}
-else{
-  this.data.getSearchByRegion(val).subscribe((result)=>{
-    console.warn(result)
-    this.dataSource=result
-    
-  })
-}
+  onChange(event: any) {
+    let val = event.target.value
+    if (val === "Filter Based on Continents") {
+      this.onFetchdata()
+    }
+    else {
+      this.data.getSearchByRegion(val).subscribe((result) => {
+        console.warn(result)
+        this.dataSource = result
+
+      })
+    }
   }
 
 }
